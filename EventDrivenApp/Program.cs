5f5ccd6de4eb991973a8d586b1911319129af5f5
Abdelrahman-Maail <abedalrhmanmaali23@gmail.com>
2025-04-13 17:34:10 +0300

    fix: add bin/obj to .gitignore and remove empty spaces in Program.cs

--- EventDrivenApp/Program.cs
+++ EventDrivenApp/Program.cs
@@ -6,19 +6,15 @@ class Program
     public static void Main(string[] args)
     {
         Publisher publisher = new Publisher();
-
         Subscriber1 sub1 = new Subscriber1("Duha");
         Subscriber2 sub2 = new Subscriber2("Alaa");
-
         Collection subscribers = new Collection();
         subscribers.AddSubscriber(sub1);
         subscribers.AddSubscriber(sub2);
-
         foreach (var sub in subscribers)
         {
             Console.WriteLine($"{sub}");
         }
-
         foreach (var sub in subscribers)
         {
             publisher.OnPrimary += sub.HandlePrimaryMessage;
@@ -26,18 +22,9 @@ class Program
             publisher.OnSocial += sub.HandleSocialMessage;
             publisher.OnUpdates += sub.HandleUpdateMessage;
         }
-
         publisher.NotifyPrimary("Your account was accessed from a new device.", DateTime.Now);
         publisher.NotifyPromotions("50% off on all items!", DateTime.Now);
         publisher.NotifySocial("You have a new friend request.", DateTime.Now);
         publisher.NotifyUpdates("This new IDE from Google is an absolute game changer | Tari Ibaba in Coding Beauty", DateTime.Now);
-
-
-
-
-
-
-
-
     }
 }
